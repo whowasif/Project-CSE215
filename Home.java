523680c23aef946d6267b36741c17bbb200e9ddf
whowasif <107785378+whowasif@users.noreply.github.com>
2022-08-27 05:08:54 +0600

    Add files via upload

---
+++ Home.java
@@ -0,0 +1,125 @@
+package Homepage;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+import Login.LoginPage;
+import Login.SignUp;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.Cursor;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Home extends JFrame {
+
+	public JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home window = new Home();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Home() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.CYAN);
+		frame.setTitle("HomePage");
+		frame.setBackground(Color.CYAN);
+		frame.setBounds(100, 100, 800, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(240, 248, 255));
+		panel.setBorder(null);			//WHOLE PANEL
+		panel.setBounds(0, 0, 485, 282);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(202, 233, 245));
+		panel_1.setBorder(new LineBorder(new Color(202, 233, 245), 2, true));		//LEFT PANEL
+		panel_1.setBounds(0,50,190,413);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Make Your Exams Easier... ");
+		lblNewLabel.setFont(new Font("Bookman Old Style", Font.BOLD, 11));
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setBounds(10, 102, 170, 43);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Than EVER...");
+		lblNewLabel_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_1.setFont(new Font("Bookman Old Style", Font.BOLD, 11));
+		lblNewLabel_1.setBounds(10, 143, 81, 13);
+		panel_1.add(lblNewLabel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(175, 220, 235), 2, true));		//UPPER PANEL
+		panel_2.setBackground(new Color(175, 220, 235));
+		panel_2.setBounds(0, 0, 785, 50);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Log In");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				LoginPage lp = new LoginPage();
+				lp.frame.setVisible(true);
+				
+			}
+		});
+		btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_1.setBorderPainted(false);
+		btnNewButton_1.setFont(new Font("Bookman Old Style", Font.BOLD, 11));
+		btnNewButton_1.setBounds(667, 19, 94, 21);
+		panel_2.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Welcome to SAW Exam App");
+		lblNewLabel_2.setFont(new Font("Century Gothic", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(322, 65, 289, 89);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Its Totally Free!!");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_3.setBounds(385, 134, 157, 38);
+		panel.add(lblNewLabel_3);
+		
+		JButton btnNewButton_1_2 = new JButton("Help");
+		btnNewButton_1_2.setBackground(new Color(192, 192, 192));
+		btnNewButton_1_2.setFont(new Font("Bookman Old Style", Font.BOLD, 11));
+		btnNewButton_1_2.setBorderPainted(false);
+		btnNewButton_1_2.setBounds(668, 432, 94, 21);
+		panel.add(btnNewButton_1_2);
+	
+	}
+}
